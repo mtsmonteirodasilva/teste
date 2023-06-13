7da05c81793d21dd487e183af7a67f22b86943d3
Matheus Monteiro <matheus2016monteiro@gmail.com>
2023-06-13 11:09:26 -0400

    teste

---
+++ Aula 3 pt 2 - P.O.O/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Programa
+{
+    static void Main(string[] args)
+    {
+        Metodos a = new Metodos();
+        double soma = a.Somar(190, 210);
+        Console.WriteLine(soma);
+
+
+        Metodos b = new Metodos();
+        double subtracao = b.Subtrair(90,87);
+        Console.WriteLine(subtracao);
+
+        Metodos c = new Metodos();
+        double multiplicacao = c.Multiplicar(60, 63);
+        Console.WriteLine(multiplicacao);
+
+        Metodos d = new Metodos();
+        double divisao = c.Divisão(100, 10);
+        Console.WriteLine(divisao);
+    }
+}
---
+++ Aula 3 pt 2 - P.O.O/metodos.cs
@@ -0,0 +1,19 @@
+﻿public class Metodos
+{
+    public double Somar(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+    public double Subtrair(double n1, double n2)
+    {
+        return n1 - n2;
+    }
+    public double Multiplicar(double n1, double n2)
+    {
+        return n1 * n2;
+    }
+    public double Divisão(double n1, double n2)
+    {
+        return n1 / n2;
+    }
+}
